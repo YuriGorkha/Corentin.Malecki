1a883876b8945fadd552d141c34b4268ee00e90b
YuriGorkha <114921833+YuriGorkha@users.noreply.github.com>
2023-07-11 13:33:04 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,14 +1,14 @@
 <?php
-    if (isset($_POST['message'])) {
-        $message = '<h1>Message envoyé depuis la page Contact de sitecv</h1>
-        <p><b>Email : </b>' . $_POST['email'] . '<br>
-        <b>Nom : </b>' . $_POST['nom'] . '<br>
-        <b>Prénom : </b>' . $_POST['prénom'] . '<br>
-        <b>Message : </b>' . htmlspecialchars($_POST['message']) . '</p>';
+if (isset($_POST['message'])) {
+    $message = 'Message envoyé depuis la page Contact de sitecv
+    Email : ' . $_POST['email'] . '
+    Nom : ' . $_POST['nom'] . '
+    Prénom : ' . $_POST['prénom'] . '
+    Message : ' . $_POST['message'];
 
-        $retour = mail('corentin.malecki@gmail.com', 'Envoi depuis page Contact', $message, 'From:contact@sitecv.fr\r\nReply-to:' . $_POST['email']);
-        if($retour) {
-            echo '<p>Votre message a bien été envoyé.</p>';
-            }
+    $retour = mail('corentin.malecki@gmail.com', 'Envoi depuis page Contact', $message, 'From:contact@sitecv.fr\r\nReply-to:' . $_POST['email']);
+    if($retour) {
+        echo 'Votre message a bien été envoyé.';
     }
-    ?>
+}
+?>
